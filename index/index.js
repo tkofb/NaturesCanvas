c245c2b34e04d485413befa65108b47211ac5cf7
tkofb <benard.kihiuria@gmail.com>
2023-07-24 23:41:49 -0400

    Done with website for the moment

--- index/index.js
+++ index/index.js
@@ -1,1 +1,12 @@
 
+if (typeof window !== "undefined"){
+
+let loginForm = document.getElementById("loginForm");
+
+
+loginForm.addEventListener("submit", (e)=>{
+    let firstName = document.getElementById('firstName');
+});
+
+console.log(firstName);
+}
